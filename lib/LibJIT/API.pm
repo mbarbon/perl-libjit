1a38e454d32b977984c03fe035e002a8415e0bb2
Mattia Barbon <mattia@barbon.org>
2013-08-10 20:29:09 +0200

    Export all types.

--- lib/LibJIT/API.pm
+++ lib/LibJIT/API.pm
@@ -40,10 +40,37 @@ my @Constants = qw(
     jit_abi_stdcall
     jit_abi_fastcall
 
+    jit_type_void
+    jit_type_sbyte
+    jit_type_ubyte
+    jit_type_short
+    jit_type_ushort
     jit_type_int
+    jit_type_uint
     jit_type_nint
+    jit_type_nuint
+    jit_type_long
+    jit_type_ulong
+    jit_type_float32
     jit_type_float64
+    jit_type_nfloat
     jit_type_void_ptr
+
+    jit_type_sys_bool
+    jit_type_sys_char
+    jit_type_sys_schar
+    jit_type_sys_uchar
+    jit_type_sys_short
+    jit_type_sys_ushort
+    jit_type_sys_int
+    jit_type_sys_uint
+    jit_type_sys_long
+    jit_type_sys_ulong
+    jit_type_sys_longlong
+    jit_type_sys_ulonglong
+    jit_type_sys_float
+    jit_type_sys_double
+    jit_type_sys_long_double
 );
 
 our @EXPORT_OK = (@Functions, @Constants);
